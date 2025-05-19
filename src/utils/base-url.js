1de7fb8ea33d8c1e87bb124ce7a12aaa9a72737d
Vlad Pohorilets <pohoriletts@gmail.com>
2025-05-19 16:22:45 +0300

    bugs fix

--- src/utils/base-url.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const API_URL = process.env.URL || 'http://localhost';
-const API_PORT = process.env.PORT || '3000';
-
-const baseURL = `${API_URL.replace(/\/+$/, '')}:${API_PORT}`;
-
-module.exports = baseURL;
